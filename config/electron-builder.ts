bce0a3e21f998382cc6104117001b9277a7d47a1
S. Davis <sdavis@nativeit.net>
2024-12-29 22:19:32 -0500

    Testing a new build

--- config/electron-builder.ts
+++ config/electron-builder.ts
@@ -47,7 +47,7 @@ export default {
   },
   linux: {
     target: ['snap', 'deb'],
-    category: 'Development Tools'
+    category: 'Development Tools',
     icon: 'config/icons'
   },
   extraMetadata: {
